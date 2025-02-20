8d070b05ea1ecabe30a5080da9e7f908f415d9f2
Your Name <you@example.com>
2025-02-20 15:54:05 -0700

    .

--- Ciclos/Program.cs
+++ Ciclos/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/*-CICLOS ASÍ INFINITOS ALV*/
+Console.WriteLine("Ciclo for:");
+for(int i=0; i<5;i++){
+    Console.WriteLine("Miguel Herrera Chngon");
+    i++;
+}
+
+Console.WriteLine("Ciclo while:");
+int w=0;
+while(w<5){
+    Console.WriteLine("Iteración: "+w);
+    w++;
+}
+
+/*-CONDICIONALES-*/
