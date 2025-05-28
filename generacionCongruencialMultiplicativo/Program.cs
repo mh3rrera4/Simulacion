10695ad77dd331ca1f945499ca5c3db35b99bd9b
Miguel Herrera <herreralag34@hotmail.com>
2025-05-27 17:07:18 -0700

    Se agrego el Generador Congruencial Multiplicativo

---
+++ generacionCongruencialMultiplicativo/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main() {
+        int seed = 17;
+        int a = 5 + 8 * 2;
+        int m = (int)Math.Pow(2, 5);
+        int cantidad = 1;
+
+        Console.WriteLine("Numeros Pseudoaleatorios generados:");
+        for(int i =0; i<cantidad; i++){
+            seed = (a* seed) % m;
+            double randomValue = (double)seed / (m - 1);
+            Console.WriteLine(randomValue);
+        }
+	
+    }
+}
